663dd7eb7c18d4727b35a87456c96dcfee317c14
Hunter Parcells <hparcells@icloud.com>
2019-01-08 18:35:37 -0500

    Fixed Web Worker

--- cracker.js
+++ cracker.js
@@ -1,20 +1,24 @@
 var crackedCard = document.getElementById("cracked-card");
 var crackedTime = document.getElementById("cracked-time");
+var crackedGuesses = document.getElementById("cracked-guesses");
+var crackingCard = document.getElementById("cracking-card");
+var crackingGuessCount = document.getElementById("guess-live-count");
 
 var startTime;
 var shouldCrack = true;
-var worker = new Worker('worker.js');
+var worker;
 
 crackedCard.style.display = "none";
+crackingCard.style.display = "none";
 
 function validate() {
     var password = document.getElementById("password").value;
 
     if(password.length > 25 || password.length === 0) {
-        M.toast({html: "Either your password is too short or too long!"});
         return;
-    }else {
+    } else {
         crackedCard.style.display = "none";
+        crackingCard.style.display = "block";
         startTime = Date.now();
         shouldCrack = true;
         worker.postMessage({
@@ -24,19 +28,37 @@ function validate() {
     }
 }
 
+function restartWorker() {
+    if(worker) worker.terminate();
 
-worker.addEventListener('message', function (e) {
-    var data = e.data;
-    if(data.action === "completed") {
+    worker = new Worker('worker.js');
 
-        // data = {guesses: Number, time: Number, password: string}
-
-        if(data.time === 0) {
-            crackedTime.innerText = "< 1"
-        } else {
-            crackedTime.innerText = data.time;
+    worker.addEventListener('message', function (e) {
+        var data = e.data;
+        if(data.action === "completed") {
+    
+            // data = {guesses: number, time: number, password: string}
+    
+            if(data.time === 0) {
+                crackedTime.innerText = "< 1";
+            }else {
+                crackedTime.innerText = data.time;
+            }
+    
+            crackedGuesses.innerText = data.guesses;
+    
+            crackedCard.style.display = "block";
+            crackingCard.style.display = "none";
+        }else if (data.action === "midway-progress") {
+            crackingGuessCount.innerText = data.guesses;
         }
+    }, false);
+}
 
-        crackedCard.style.display = "block";
-    }
-}, false);
+restartWorker();
+
+function stop() {
+    crackedCard.style.display = "none";
+    crackingCard.style.display = "none";
+    restartWorker();
+}
